2b8d314a7300d51c45e3d355625f170ff2f2ddc2
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-12 08:50:44 +0400

    added application warmup

--- src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
+++ src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
@@ -4,11 +4,11 @@ import az.fitnest.notifications.configuration.LsimSmsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 @EnableJpaAuditing
 @EnableConfigurationProperties(LsimSmsProperties.class)
 public class NotificationsServiceApplication {
