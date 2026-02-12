890349fd1a030dc0e4fe0d36d02dcd6f3baae94a
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-12 08:51:17 +0400

    added notifications application warmup

--- src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
+++ src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
@@ -4,6 +4,8 @@ import az.fitnest.notifications.configuration.LsimSmsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
