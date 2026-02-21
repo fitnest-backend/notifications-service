70232d00af2230253f164d638f8ac4a54bc9807c
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-22 00:48:43 +0400

    picture fix

--- src/main/java/az/fitnest/notifications/entity/Notification.java
+++ src/main/java/az/fitnest/notifications/entity/Notification.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
