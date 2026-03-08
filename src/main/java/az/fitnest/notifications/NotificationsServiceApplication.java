4c10f30b73621958948d2aacb162e0ae399f5fe0
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-08 21:31:08 +0400

    fix: enable Kafka support and set default JSON value type for NotificationEvent

--- src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
+++ src/main/java/az/fitnest/notifications/NotificationsServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableKafka
 @EnableJpaAuditing
 @EnableConfigurationProperties(LsimSmsProperties.class)
 public class NotificationsServiceApplication {
