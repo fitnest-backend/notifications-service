95924137fce1addf4327e4289c0b8177ae13457c
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-06-02 20:39:17 +0400

    fix: change javax to jakarta PostConstruct annotation for Spring Boot 4 compatibility

--- src/main/java/az/fitnest/notifications/configuration/FirebaseConfig.java
+++ src/main/java/az/fitnest/notifications/configuration/FirebaseConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
